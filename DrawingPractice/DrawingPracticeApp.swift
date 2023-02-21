805eb1238f2c91f04e53889d711da84d7086c831
Berkay-Disli <berkaydisli.work@gmail.com>
2023-02-22 00:56:10 +0300

    Initial Commit

---
+++ DrawingPractice/DrawingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingPracticeApp.swift
+//  DrawingPractice
+//
+//  Created by Berkay Disli on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
